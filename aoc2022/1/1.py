4448fbcd5d42ab6bc8eb327f73ae576f4a78dd55
Andrei Dukhounik <dukhouni@amazon.com>
2022-11-30 21:06:32 -0800

    2022 day 1

---
+++ aoc2022/1/1.py
@@ -0,0 +1,17 @@
+res = []
+cur = 0
+with open('in.in', 'r') as f:
+    for l in f.readlines():
+        l = l.strip()
+        if len(l) == 0:
+            res.append(cur)
+            cur = 0
+        else:
+            cur += int(l)
+
+res.append(cur)
+# part 1
+print(max(res))
+# part 2
+res.sort(reverse=True)
+print(sum(res[:3]))
